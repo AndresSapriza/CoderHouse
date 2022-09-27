61036bf23de68146a50d15cf26b69e166fd3d74e
AndresSapriza <andresapriza@hotmail.com>
2022-09-27 19:27:12 -0300

    cart route added

---
+++ ProyectoFinal/routes/cart.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const cartRouter = express.Router();
+
+
+cartRouter.get('/:id/products', (req, res) => {
+    res.status(404).send('Sorry, we are working on it!');
+});
+
+cartRouter.post('/', (req, res) => {
+    res.status(404).send('Sorry, we are working on it!');    
+});
+
+cartRouter.post('/:id/products', (req, res) => {
+    res.status(404).send('Sorry, we are working on it!');    
+});
+
+cartRouter.delete('/:id', (req, res) => {
+    res.status(404).send('Sorry, we are working on it!');
+});
+
+cartRouter.delete('/:id/productos/:id_prod', (req, res) => {
+    res.status(404).send('Sorry, we are working on it!');
+});
+
+
+export default cartRouter;
